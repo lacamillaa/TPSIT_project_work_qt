a3fd2a0b16263d85afea85126473a5396d2b2b04
lacamillaa <12999356@itisrossi.vi.it>
2026-05-04 12:15:40 +0200

    restituzione del contenuto HTML della pagina di Spotify

--- main.cpp
+++ main.cpp
@@ -1,24 +1,11 @@
 #include "mainwindow.h"
 
 #include <QApplication>
-#include <QUrl>
-#include <QtNetwork/QNetworkAccessManager>
-#include <QtNetwork/QNetworkRequest>
-#include <QtNetwork/QNetworkReply>
-#include <QJsonDocument>
-#include <QJsonObject>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
-    QNetworkAccessManager m;
-    QNetworkRequest req(QUrl("http://localhost:3000/auth"));
-    QNetworkReply *reply = m.get(req);
-    QByteArray resp = reply->readAll();
-    QJsonDocument doc = QJsonDocument::fromJson(resp);
-    QJsonObject obj = doc.object();
-    qDebug() << "token: " << obj["access_token"].toString();
     w.show();
     return QCoreApplication::exec();
 }
--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,36 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
 
+#include <QMainWindow>
+#include <QUrl>
+#include <QtNetwork/QNetworkAccessManager>
+#include <QtNetwork/QNetworkRequest>
+#include <QtNetwork/QNetworkReply>
+#include <QJsonDocument>
+#include <QJsonObject>
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    QNetworkAccessManager m;
+    char APIurl[] = "http://localhost:3000/auth";
+    QNetworkRequest req((QUrl(APIurl)));
+    QNetworkReply *reply = m.get(req);
+
+    QEventLoop loop;
+    connect(reply, &QNetworkReply::finished, &loop, &QEventLoop::quit);
+    loop.exec();
+
+    if (reply->error() == QNetworkReply::NoError) {
+        QString resp = reply->readAll();
+        qDebug() << resp;
+    }
+    else {
+        qDebug() << "Errore: " << reply->errorString();
+    }
 }
 
 MainWindow::~MainWindow()
